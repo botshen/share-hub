2f3246d12259306984a81d3508df5e64244ae587
Xin shen <363088847@qq.com>
2025-02-20 22:49:23 +0800

    chore: 重构项目结构，将源代码移至 src 目录

---
+++ src/entrypoints/options/App.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const handleMessage = (message: any) => {
+  console.log("message", message);
+};
+browser.runtime.onMessage.addListener(handleMessage);
+</script>
+
+<template>
+  <div>
+    <h1 class="text-6xl font-bold text-red-600">Options</h1>
+    <button class="btn">
+      <span class="loading loading-spinner"></span>
+      loading
+    </button>
+  </div>
+</template>
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,10 +1,30 @@
 import { defineConfig } from 'wxt';
+import Icons from "unplugin-icons/vite";
+import Tailwindcss from "@tailwindcss/vite";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  srcDir: "src", 
   extensionApi: 'chrome',
-  modules: ['@wxt-dev/module-vue'],
+  modules: [
+    "@wxt-dev/module-vue",
+    "@wxt-dev/i18n/module",
+    "@wxt-dev/auto-icons",
+  ],  
   runner: {
     disabled: true,
+  },
+  vite: () => ({
+    plugins: [Icons({ compiler: "vue3" }), Tailwindcss() as any],
+    ssr: {
+      // List any dependencies that depend on webextension-polyfill here for vite-node importer to work
+      noExternal: ["@webext-core/proxy-service", "@webext-core/messaging"],
+    },
+  }),
+  manifest: {
+    permissions: ['storage'],
+  },
+  autoIcons: {
+    grayscaleOnDevelopment: false,
   },
 });
