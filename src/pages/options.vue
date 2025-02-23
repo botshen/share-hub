47611fecb06963089eb4f9a17d4393c280f166d4
Xin shen <363088847@qq.com>
2025-02-23 22:29:26 +0800

    feat: 图片生成

--- src/pages/options.vue
+++ src/pages/options.vue
@@ -3,8 +3,8 @@ import { generateImage } from "@/utils/image";
 import { useOptionsStore } from "./use-options-store";
 import ColorPicker, { ColorPreset } from "@/components/ColorPicker.vue";
 import { onMessage } from "@/utils/message";
- 
-const { todos, getTodos, deleteTodo,currentTodoId } =
+
+const { todos, getTodos, deleteTodo, currentTodoId } =
   useOptionsStore();
 
 onMounted(async () => {
@@ -16,7 +16,7 @@ onMessage("openOptionsPage", async () => {
 
 const handlePresetSelect = (preset: ColorPreset) => {
   console.log("选中的预设:", preset);
- };
+};
 
 const handleSelectTodo = (todoId: number) => {
   currentTodoId.value = todoId;
@@ -25,9 +25,9 @@ const handleDelete = (todoId: number) => {
   deleteTodo(todoId);
 };
 const currentTodo = computed(() => {
-   if (!todos.value) return null;
+  if (!todos.value) return null;
   console.log("todos.value", todos.value);
-  console.log('currentTodoId',currentTodoId)
+  console.log('currentTodoId', currentTodoId)
   if (!currentTodoId.value) return todos.value?.[0];
   return todos.value?.find((todo) => todo.id === currentTodoId.value);
 });
@@ -65,11 +65,11 @@ const postFontSizeMap = {
   '3xl': '32px',
 } as const;
 
- 
+
 
 type WidthSize = keyof typeof widthMap;
 type FontSize = keyof typeof postFontSizeMap;
- type TitleFontSize = keyof typeof titleFontSizeMap;
+type TitleFontSize = keyof typeof titleFontSizeMap;
 const config = ref({
   width: 'md' as WidthSize,
   padding: 'md',
@@ -78,7 +78,7 @@ const config = ref({
   imageQuality: '2'
 });
 
-const detailCss = (newVal: typeof config.value)=>{
+const detailCss = (newVal: typeof config.value) => {
   console.log("config", newVal);
   const card = document.getElementById('card')
   if (card) {
@@ -95,11 +95,12 @@ const detailCss = (newVal: typeof config.value)=>{
   if (title) {
     title.style.fontSize = titleFontSizeMap[newVal.fontSize as TitleFontSize]
   }
+
 }
 watch(config, (newVal) => {
   detailCss(newVal)
 }, { deep: true, immediate: true });
- 
+
 onMounted(() => {
   console.log("mounted");
   detailCss(config.value)
@@ -170,17 +171,17 @@ const onDragEnd = () => {
 // 4. 动画配置
 const commentMotion = {
   initial: { scale: 1, rotate: 0 },
-  hover: { 
+  hover: {
     scale: 1.02,
     transition: { duration: 200 }
   },
-  dragging: { 
-    scale: 1.05, 
+  dragging: {
+    scale: 1.05,
     rotate: 3,
-    transition: { 
+    transition: {
       type: "spring",
       stiffness: 300,
-      damping: 20 
+      damping: 20
     }
   }
 };
@@ -194,74 +195,72 @@ const commentMotion = {
   </div>
   <div class="flex bg-cover px-[100px] mt-2">
     <ul class="list bg-base-100 rounded-box shadow-md shrink-0 h-auto w-[300px]">
-      <li v-for="todo in todos" :key="todo.id" >
+      <li v-for="todo in todos" :key="todo.id">
         <div @click="handleSelectTodo(todo.id)" class="list-row cursor-pointer">
           <div class="mask mask-squircle w-10 h-10">
-          <img :src="todo.avatarUrl" />
-        </div>
-        <div>
-          <div>{{ todo.author }}</div>
-          <div class="text-xs uppercase font-semibold opacity-60">
-            {{ todo.title }}
+            <img :src="todo.avatarUrl" />
           </div>
+          <div>
+            <div>{{ todo.author }}</div>
+            <div class="text-xs uppercase font-semibold opacity-60">
+              {{ todo.title }}
+            </div>
+          </div>
+
+          <button v-if="todos.length > 1" class="btn btn-square btn-ghost" @click="handleDelete(todo.id)">
+            <svg t="1740274258800" class="icon size-[1.2em]" viewBox="0 0 1024 1024" version="1.1"
+              xmlns="http://www.w3.org/2000/svg" p-id="7931" width="200" height="200">
+              <path
+                d="M849.292759 73.484087H634.508149v-13.831973A59.650385 59.650385 0 0 0 574.46874 0.001729H449.462282a59.650385 59.650385 0 0 0-59.650385 59.650385v13.831973H174.595038A62.373554 62.373554 0 0 0 111.48666 133.566721a60.947132 60.947132 0 0 0 60.947132 61.89808h679.063438a60.947132 60.947132 0 0 0 60.947132-61.89808 62.373554 62.373554 0 0 0-63.151603-60.082634"
+                fill="#FF9000" p-id="7932"></path>
+              <path
+                d="M812.032881 233.632402H211.854916A72.747534 72.747534 0 0 0 139.496406 313.728173l62.546454 567.845727a158.332869 158.332869 0 0 0 157.511595 142.382875h304.735662a158.332869 158.332869 0 0 0 157.511595-142.382875l62.41678-567.845727a72.747534 72.747534 0 0 0-72.358511-80.095771m-363.521547 536.291539a42.662993 42.662993 0 0 1-42.662993 42.662993h-6.656637a42.662993 42.662993 0 0 1-42.662992-42.662993v-330.843511a42.662993 42.662993 0 0 1 42.662992-42.662992h6.656637a42.662993 42.662993 0 0 1 42.662993 42.662992z m219.236776 0a42.662993 42.662993 0 0 1-42.662992 42.662993h-6.656637a42.662993 42.662993 0 0 1-42.662993-42.662993v-330.843511a42.662993 42.662993 0 0 1 42.662993-42.662992h6.613412a42.662993 42.662993 0 0 1 42.662992 42.662992z"
+                fill="#F61818" p-id="7933"></path>
+            </svg>
+          </button>
         </div>
- 
-        <button v-if="todos.length > 1" class="btn btn-square btn-ghost" @click="handleDelete(todo.id)">
-          <svg t="1740274258800" class="icon size-[1.2em]" viewBox="0 0 1024 1024" version="1.1"
-            xmlns="http://www.w3.org/2000/svg" p-id="7931" width="200" height="200">
-            <path
-              d="M849.292759 73.484087H634.508149v-13.831973A59.650385 59.650385 0 0 0 574.46874 0.001729H449.462282a59.650385 59.650385 0 0 0-59.650385 59.650385v13.831973H174.595038A62.373554 62.373554 0 0 0 111.48666 133.566721a60.947132 60.947132 0 0 0 60.947132 61.89808h679.063438a60.947132 60.947132 0 0 0 60.947132-61.89808 62.373554 62.373554 0 0 0-63.151603-60.082634"
-              fill="#FF9000" p-id="7932"></path>
-            <path
-              d="M812.032881 233.632402H211.854916A72.747534 72.747534 0 0 0 139.496406 313.728173l62.546454 567.845727a158.332869 158.332869 0 0 0 157.511595 142.382875h304.735662a158.332869 158.332869 0 0 0 157.511595-142.382875l62.41678-567.845727a72.747534 72.747534 0 0 0-72.358511-80.095771m-363.521547 536.291539a42.662993 42.662993 0 0 1-42.662993 42.662993h-6.656637a42.662993 42.662993 0 0 1-42.662992-42.662993v-330.843511a42.662993 42.662993 0 0 1 42.662992-42.662992h6.656637a42.662993 42.662993 0 0 1 42.662993 42.662992z m219.236776 0a42.662993 42.662993 0 0 1-42.662992 42.662993h-6.656637a42.662993 42.662993 0 0 1-42.662993-42.662993v-330.843511a42.662993 42.662993 0 0 1 42.662993-42.662992h6.613412a42.662993 42.662993 0 0 1 42.662992 42.662992z"
-              fill="#F61818" p-id="7933"></path>
-          </svg>
-        </button>
-        </div>
-        
-     
+
+
       </li>
     </ul>
     <main class="mx-auto bg-white w-[448px] border rounded-box p-4 py-6 m-4" id="card">
       <header class="flex items-center justify-between gap-2">
         <div class="flex items-center gap-2">
           <div class="avatar">
-          <div class="mask mask-squircle w-10">
-            <img :src="currentTodo?.avatarUrl" />
+            <div class="mask mask-squircle w-10">
+              <img :src="currentTodo?.avatarUrl" />
+            </div>
           </div>
-        </div>
-        <span class="text-lg">{{ currentTodo?.author }}</span>
+          <span class="text-lg">{{ currentTodo?.author }}</span>
         </div>
         <div class="flex items-center gap-2 text-gray-500">
           V2ex
-          <svg t="1740317552775" class="icon size-8 opacity-30" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2627" width="200" height="200"><path d="M46.08 47.616c-6.656 8.192-10.24 62.976-10.24 179.712v167.424h542.72l44.544 52.736c24.576 28.672 44.544 58.88 44.544 67.584 0 8.192-20.48 37.888-45.056 67.072l-45.056 51.712H35.84v160.768c0 90.624 3.584 168.448 8.704 179.712 8.704 17.92 21.504 18.944 298.496 18.944 158.72 0 295.424-3.072 302.08-6.144 7.68-3.072 92.16-104.96 188.416-224.256 149.504-186.368 175.104-222.208 175.104-248.32s-18.944-54.784-116.736-177.664C827.904 256 746.496 155.648 710.656 112.64L646.144 35.84H351.744C133.12 35.84 53.76 38.912 46.08 47.616z" fill="#707070" p-id="2628"></path></svg>
+          <svg t="1740317552775" class="icon size-8 opacity-30" viewBox="0 0 1024 1024" version="1.1"
+            xmlns="http://www.w3.org/2000/svg" p-id="2627" width="200" height="200">
+            <path
+              d="M46.08 47.616c-6.656 8.192-10.24 62.976-10.24 179.712v167.424h542.72l44.544 52.736c24.576 28.672 44.544 58.88 44.544 67.584 0 8.192-20.48 37.888-45.056 67.072l-45.056 51.712H35.84v160.768c0 90.624 3.584 168.448 8.704 179.712 8.704 17.92 21.504 18.944 298.496 18.944 158.72 0 295.424-3.072 302.08-6.144 7.68-3.072 92.16-104.96 188.416-224.256 149.504-186.368 175.104-222.208 175.104-248.32s-18.944-54.784-116.736-177.664C827.904 256 746.496 155.648 710.656 112.64L646.144 35.84H351.744C133.12 35.84 53.76 38.912 46.08 47.616z"
+              fill="#707070" p-id="2628"></path>
+          </svg>
         </div>
       </header>
       <x-title class="font-bold block my-2 p-2" id="title">{{
         currentTodo?.title
-      }}</x-title>
+        }}</x-title>
       <div class="  p-2 cursor-pointer whitespace-pre-wrap break-words" :contentEditable="true"
         :innerHTML="currentTodo?.postContent" />
       <div class="comments-container">
         <!-- 精选区域 -->
-        <div class="selected-zone" 
-             :class="{ 'is-active': isDragging }">
+        <div class="selected-zone" :class="{ 'is-active': isDragging }">
           <div class="selected-zone-header flex items-center justify-between">
             <span class="text-lg font-bold">精选评论</span>
-            <span class="text-sm text-gray-500">拖动评论到此处添加到精选</span>
+            <span class="text-sm text-gray-500" v-if="!isCopying && !isDownloading">拖动评论到此处添加到精选</span>
           </div>
-          <VueDraggable 
-            v-model="selectedComments"
-            group="comments"
-            @start="onDragStart"
-            @end="onDragEnd"
-            item-key="id"
+          <VueDraggable v-model="selectedComments" group="comments" @start="onDragStart" @end="onDragEnd" item-key="id"
             class="selected-list">
             <template #item="{ element }">
               <div>
                 <div class="divider h-1"></div>
-                <div class="selected-comment" 
-                     v-motion="commentMotion">
+                <div class="selected-comment" v-motion="commentMotion">
                   <div class="comment-content">
                     <div class="flex items-center gap-2 mb-2">
                       <div class="mask mask-squircle w-8">
@@ -278,16 +277,10 @@ const commentMotion = {
         </div>
 
         <!-- 普通评论区 -->
-        <VueDraggable 
-          :list="currentTodo?.comments"
-          group="comments"
-          @start="onDragStart"
-          @end="onDragEnd"
-          item-key="id"
-          class="comments-list">
+        <VueDraggable v-if="!isCopying && !isDownloading" :list="currentTodo?.comments" group="comments"
+          @start="onDragStart" @end="onDragEnd" item-key="id" class="comments-list">
           <template #item="{ element }">
-            <div class="comment-item" 
-                 v-motion="commentMotion">
+            <div class="comment-item" v-motion="commentMotion">
               <x-comment id="comment" class="px-2 block">
                 <div class="flex items-center gap-2">
                   <div class="mask mask-squircle w-8">
@@ -295,7 +288,8 @@ const commentMotion = {
                   </div>
                   <span class="font-bold">{{ element.author }}</span>
                 </div>
-                <div class="break-words whitespace-pre-wrap overflow-wrap-anywhere ml-10 mt-1" v-html="element.content" />
+                <div class="break-words whitespace-pre-wrap overflow-wrap-anywhere ml-10 mt-1"
+                  v-html="element.content" />
               </x-comment>
             </div>
           </template>
@@ -350,7 +344,7 @@ const commentMotion = {
         </span>
         Download</button>
       <button class="btn btn-block" @click="handleCopy">
-        <span v-if="isCopying" class="loading loading-spinner">
+        <span v-if="isCopying && !isDownloading" class="loading loading-spinner">
         </span>
         Copy</button>
     </x-action-bar>
@@ -366,7 +360,7 @@ const commentMotion = {
 
 .selected-zone {
   min-height: 100px;
-  border: 2px dashed #ddd;
+  border: v-bind('(isCopying || isDownloading) ? "none" : "2px dashed #ddd"');
   border-radius: 8px;
   padding: 1rem;
   transition: all 0.3s ease;
@@ -378,12 +372,10 @@ const commentMotion = {
   content: '';
   position: absolute;
   inset: -2px;
-  background: linear-gradient(
-    90deg,
-    #4CAF50,
-    #2196F3,
-    #4CAF50
-  );
+  background: linear-gradient(90deg,
+      #4CAF50,
+      #2196F3,
+      #4CAF50);
   border-radius: 10px;
   z-index: -1;
   opacity: 0;
@@ -402,24 +394,27 @@ const commentMotion = {
   opacity: 0.2;
 }
 
-.selected-comment, .comment-item {
+.selected-comment,
+.comment-item {
   position: relative;
   padding: 1rem;
   margin: 0.5rem 0;
   background: white;
   border-radius: 8px;
-  box-shadow: 0 2px 8px rgba(0,0,0,0.1);
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
   cursor: grab;
   transition: all 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
   will-change: transform;
 }
 
-.selected-comment:hover, .comment-item:hover {
-  box-shadow: 0 8px 16px rgba(0,0,0,0.1);
+.selected-comment:hover,
+.comment-item:hover {
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);
   transform: translateY(-2px) rotate(1deg);
 }
 
-.selected-comment:active, .comment-item:active {
+.selected-comment:active,
+.comment-item:active {
   cursor: grabbing;
 }
 
@@ -437,7 +432,8 @@ const commentMotion = {
   background: white !important;
 }
 
-.selected-list, .comments-list {
+.selected-list,
+.comments-list {
   min-height: 50px;
 }
 
@@ -445,9 +441,11 @@ const commentMotion = {
   0% {
     background-position: 0% 50%;
   }
+
   50% {
     background-position: 100% 50%;
   }
+
   100% {
     background-position: 0% 50%;
   }
@@ -458,10 +456,12 @@ const commentMotion = {
     box-shadow: 0 12px 24px rgba(76, 175, 80, 0.3);
     transform: scale(1.05) rotate(3deg);
   }
+
   50% {
     box-shadow: 0 16px 32px rgba(76, 175, 80, 0.4);
     transform: scale(1.08) rotate(3deg);
   }
+
   100% {
     box-shadow: 0 12px 24px rgba(76, 175, 80, 0.3);
     transform: scale(1.05) rotate(3deg);
